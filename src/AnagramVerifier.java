589a58726b1aba1d26c469303184579d0e9a628a
antuan.yanez <antuan.yanez@gmail.com>
2016-06-22 20:21:13 -0600

    list of anagrams in array, and count of elements of each one

--- src/AnagramVerifier.java
+++ src/AnagramVerifier.java
@@ -1,17 +1,47 @@
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * Created by Usuario on 21/06/2016.
  */
 public class AnagramVerifier {
 
-    public boolean areAnagrams(String firstWord,String secondWord){
+    public boolean areAnagrams(String firstWord, String secondWord) {
         char[] firstWordArray = firstWord.toCharArray();
         char[] seconfWordArray = secondWord.toCharArray();
 
         Arrays.sort(firstWordArray);
         Arrays.sort(seconfWordArray);
 
-        return Arrays.equals(firstWordArray,seconfWordArray);
+        return Arrays.equals(firstWordArray, seconfWordArray);
     }
+
+    public Map<String, List> getAnagrams(String[] arrayTestString) {
+
+        Map<String, List> mapTest = new HashMap<String, List>();
+
+        for (String word : arrayTestString) {
+            char[] wordArray = word.toCharArray();
+            Arrays.sort(wordArray);
+            if (mapTest.containsKey(String.valueOf(wordArray))) {
+                mapTest.get(String.valueOf(wordArray)).add(word);
+            } else {
+                mapTest.put(String.valueOf(wordArray), new ArrayList<>(Arrays.asList(word)));
+            }
+        }
+
+
+        Iterator<Map.Entry<String, List>> it = mapTest.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, List> e = it.next();
+            String key = e.getKey();
+            List value = e.getValue();
+            if (value.size() == 1) {
+                it.remove();
+            }
+        }
+
+
+        return mapTest;
+    }
+
 }
--- src/AnagramVerifierTest.java
+++ src/AnagramVerifierTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import java.util.*;
+
 import static org.junit.Assert.*;
 
 /**
@@ -8,15 +10,46 @@ import static org.junit.Assert.*;
 public class AnagramVerifierTest {
     @Test
     public void testTwoStringsAreAnagrams(){
-        AnagramVerifier anagranVerifier = new AnagramVerifier();
-        assertTrue(anagranVerifier.areAnagrams("car","arc"));
-        assertTrue(anagranVerifier.areAnagrams("rac","arc"));
+        AnagramVerifier anagramVerifier = new AnagramVerifier();
+        assertTrue(anagramVerifier.areAnagrams("car", "arc"));
+        assertTrue(anagramVerifier.areAnagrams("rac", "arc"));
     }
 
     @Test
     public void testTwoStringsAreNotAnagrams(){
-        AnagramVerifier anagranVerifier = new AnagramVerifier();
-        assertFalse(anagranVerifier.areAnagrams("car","abc"));
-        assertFalse(anagranVerifier.areAnagrams("mer","arc"));
+        AnagramVerifier anagramVerifier = new AnagramVerifier();
+        assertFalse(anagramVerifier.areAnagrams("car", "abc"));
+        assertFalse(anagramVerifier.areAnagrams("mer", "arc"));
+    }
+
+
+    @Test
+    public void testListOfAnagrams(){
+        AnagramVerifier anagramVerifier = new AnagramVerifier();
+        List<String> listOfAnagrams = new ArrayList<>(Arrays.asList("car","rac","arc"));
+        String[] arrayTestString ={"abc","car","mer","rac","arc"};
+        Map<String, List> mapTest = new HashMap<String, List>();
+        mapTest.put("acr", listOfAnagrams);
+        Map<String, List> resultMapSingleAnagram =anagramVerifier.getAnagrams(arrayTestString);
+        assertTrue(mapTest.equals(resultMapSingleAnagram));
+        assertEquals(3,resultMapSingleAnagram.get("acr").size());
+
+        List<String> listOfacrAnagrams = new ArrayList<>(Arrays.asList("car","rac","arc"));
+        List<String> listOfAmrAnagrams = new ArrayList<>(Arrays.asList("mar","amr"));
+        String[] arrayWithTwoListOfAnagrams ={"abc","car","mer","rac","arc","mar","amr"};
+        Map<String, List> mapTestTwoAnagrams = new HashMap<String, List>();
+        mapTestTwoAnagrams.put("acr", listOfacrAnagrams);
+        mapTestTwoAnagrams.put("amr", listOfAmrAnagrams);
+
+        Map<String, List> resultMapTwoAnagram = anagramVerifier.getAnagrams(arrayWithTwoListOfAnagrams);
+        assertTrue(mapTestTwoAnagrams.equals(resultMapTwoAnagram));
+
+        assertEquals(3, resultMapTwoAnagram.get("acr").size());
+        assertEquals(2,resultMapTwoAnagram.get("amr").size());
+
+        String[] arrayNoAnyAnagram ={"abc","def","ghi","jkl","mnb"};
+        assertTrue((new HashMap<String, List>()).equals(anagramVerifier.getAnagrams(arrayNoAnyAnagram)));
+
+
     }
 }
